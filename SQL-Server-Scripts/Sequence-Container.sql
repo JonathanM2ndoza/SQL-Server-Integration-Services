c42f0cf883864d25910575a90db48830f33be2fe
jmendoza <jonathan.m2ndoza@gmail.com>
2020-11-09 21:37:21 -0400

    Sequence container

---
+++ SQL-Server-Scripts/Sequence-Container.sql
@@ -0,0 +1,6 @@
+--Create table Demo(ID int identity,Categoria varchar(15),Monto float)
+--insert into Demo(Categoria, Monto)
+--values ('BBB',743.23),('MMM',456.32),('UUU',644.23),('YYY',4754.32),('III',865.23),('QQQ',74.32),('DDD',3.23),('SSS',34.32)
+select * from dbo.Demo
+
+--Create table DemoDest(ID int identity,Categoria varchar(15),Monto float)
