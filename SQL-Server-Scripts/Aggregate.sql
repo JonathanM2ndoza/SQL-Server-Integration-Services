25b65aa99a8c2393ad14ad11f8d90fb9058b15ec
jmendoza <jonathan.m2ndoza@gmail.com>
2020-11-15 18:55:17 -0400

    Data-Flow-Conditional-Split

--- SQL-Server-Scripts/Aggregate.sql
+++ SQL-Server-Scripts/Aggregate.sql
@@ -1,3 +1,13 @@
 create table Promedios(PiePref varchar(10),PromedioPesoKG float)
 
 create table JugadoresPorNacionalidades(Nacionalidad varchar(100),CantidadJugadores int)
+
+
+select * from promedios;
+select * from  JugadoresPorNacionalidades;
+
+
+
+
+
+
---
+++ SQL-Server-Scripts/Conditional-Split.sql
@@ -0,0 +1,7 @@
+select top 0 ID,NombreCompleto,FechaNacimiento 
+into Mayores1990 
+from FIFA
+
+select top 0 ID,NombreCompleto,FechaNacimiento 
+into Menores1990 
+from FIFA
