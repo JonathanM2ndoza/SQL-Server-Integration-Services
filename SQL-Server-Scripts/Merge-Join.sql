e7eef4361a1ea1349e9ad5a1a87748314174d8f0
jmendoza <jonathan.m2ndoza@gmail.com>
2020-11-18 20:06:34 -0400

    Data-Flow-Merge-Join

---
+++ SQL-Server-Scripts/Merge-Join.sql
@@ -0,0 +1,8 @@
+select top 0 * into EdadNacimientoJugadores from EdadJugadores
+
+alter table EdadNacimientoJugadores add FechaNacimiento date
+
+
+select * from EdadJugadores
+select * from EdadNacimientoJugadores
+
