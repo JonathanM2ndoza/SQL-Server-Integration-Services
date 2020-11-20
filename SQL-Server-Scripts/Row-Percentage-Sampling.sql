9519c81ef2f7f3386f7bee9e0e61d02cd16d9856
jmendoza <jonathan.m2ndoza@gmail.com>
2020-11-19 21:14:36 -0400

    Data-Flow-Row-Percentage-Sampling

---
+++ SQL-Server-Scripts/Row-Percentage-Sampling.sql
@@ -0,0 +1,14 @@
+create table AleatoriosAbsolutos(ID Int,Nombre varchar(150));
+
+create table AleatoriosAbsolutosNoElegidos(ID Int,Nombre varchar(150));
+
+create table AleatoriosRelativos(ID Int,Nombre varchar(150));
+
+create table AleatoriosRelativosNoElegidos(ID Int, Nombre varchar(150));
+
+
+select * from AleatoriosAbsolutos
+select * from AleatoriosAbsolutosNoElegidos
+select * from AleatoriosRelativos
+select * from AleatoriosRelativosNoElegidos
+
